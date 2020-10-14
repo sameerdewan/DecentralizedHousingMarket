e757bc041182b2aee165262038369dd153262c96
sameerdewan <sameer.dewan@itential.com>
2020-10-14 07:36:00 -0400

    more zokrates work

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -7,6 +7,8 @@ import "./ERC721Mintable.sol";
 contract SolnSquareVerifier is HouseToken {
 
     using Counters for Counters.Counter;
+    
+    Counters.Counter private solutionsCount;
     // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
     Verifier private _verifier;
     // TODO define a solutions struct that can hold an index & an address
@@ -25,13 +27,42 @@ contract SolnSquareVerifier is HouseToken {
     // TODO Create an event to emit when a solution is added
     event SolutionAdded(uint256 solutionIndex, address solutionAddress);
 
+    constructor(address verifierAddress) public {
+        _verifier = Verifier(verifierAddress);
+    }
     // TODO Create a function to add the solutions to the array and emit the event
-
-
+    function addSolution(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c, uint[2] memory input
+    ) public {
+        bytes32 solutionHash = keccak256(abi.encodePacked(input[0], input[1]));
+        require(solutions_mapping[solutionHash].account == address(0), 'Error: Not a new solution.');
+         bool verified = _verifier.verifyTx(a, b, c, input);
+         require(verified, 'Error: Not verified.');
+         solutions_arr.push(Solution({ index: solutionsCount.current(), account: msg.sender, minted: false }));
+         emit SolutionAdded(solutionsCount.current(), msg.sender);
+    }
 
     // TODO Create a function to mint new NFT only after the solution has been verified
-    //  - make sure the solution is unique (has not been used before)
-    //  - make sure you handle metadata as well as tokenSuplly
+    function mintNFT(uint a, uint b, address to) public {
+        //  - make sure the solution is unique (has not been used before)
+        bytes32 solutionHash = keccak256(abi.encodePacked(a, b));
+        require(solutions_mapping[solutionHash].account != address(0), 'Error: Non-existent Solution.');
+        //  - make sure you handle metadata as well as tokenSuplly
+        require(solutions_mapping[solutionHash].minted == false, 'Error: Already Minted.');
+        require(solutions_mapping[solutionHash].account == msg.sender, 'Error: MSG SENDER is not account for solution hash.');
+        super.mint(to, solutions_mapping[solutionHash].index);
+        solutions_mapping[solutionHash].minted = true;
+    }
+}
+
+contract Verifier {
+    function verifyTx(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c, uint[2] memory input
+    ) public view returns (bool r) {}
 }
 
 
