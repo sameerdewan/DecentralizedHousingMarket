3cb2951bb59df6e7516b5ce0b88a69346f6f8d2f
sameerdewan <sameer.dewan@itential.com>
2020-10-14 06:46:07 -0400

    add zokrates changes

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -3,25 +3,27 @@ pragma solidity >=0.4.21 <0.6.0;
 import 'openzeppelin-solidity/contracts/drafts/Counters.sol';
 import "./ERC721Mintable.sol";
 
-// TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+// TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
 contract SolnSquareVerifier is HouseToken {
-    // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
 
-
-
+    using Counters for Counters.Counter;
+    // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+    Verifier private _verifier;
     // TODO define a solutions struct that can hold an index & an address
-
+    struct Solution {
+        uint256 index;
+        address account;
+        bool minted;
+    }
 
     // TODO define an array of the above struct
-
+    Solution[] private solutions_arr;
 
     // TODO define a mapping to store unique solutions submitted
-
-
+    mapping(bytes32 => Solution) private solutions_mapping;
 
     // TODO Create an event to emit when a solution is added
-
-
+    event SolutionAdded(uint256 solutionIndex, address solutionAddress);
 
     // TODO Create a function to add the solutions to the array and emit the event
 
