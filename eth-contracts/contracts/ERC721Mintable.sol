f36f181954807f9e7b5204d303e2c32b50ba18d7
sameerdewan <sameer.dewan@itential.com>
2020-10-14 06:20:14 -0400

    finalize housetoken contract

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -524,13 +524,23 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
 }
 
 //  TODO's: Create CustomERC721Token contract that inherits from the ERC721Metadata contract. You can name this contract as you please
-//  1) Pass in appropriate values for the inherited ERC721Metadata contract
-//      - make the base token uri: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/
-//  2) create a public mint() that does the following:
-//      -can only be executed by the contract owner
-//      -takes in a 'to' address, tokenId, and tokenURI as parameters
-//      -returns a true boolean upon completion of the function
-//      -calls the superclass mint and setTokenURI functions
-
+contract HouseToken is ERC721Metadata {
+    string private _name = 'HouseToken';
+    string private _symbol = 'HOUSE';
+    //  1) Pass in appropriate values for the inherited ERC721Metadata contract
+    //      - make the base token uri: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/
+    string private _baseTokenURI = 'https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/';
+    constructor() public ERC721Metadata(_name, _symbol, _baseTokenURI) {}
+    //  2) create a public mint() that does the following:
+    function mint(address to, uint256 tokenId) public onlyOwner returns(bool) {
+        //      -can only be executed by the contract owner
+        //      -takes in a 'to' address, tokenId, and tokenURI as parameters
+        //      -returns a true boolean upon completion of the function
+        //      -calls the superclass mint and setTokenURI functions
+        _mint(to, tokenId);
+        setTokenURI(tokenId);
+        return true;
+    }
+}
 
 
