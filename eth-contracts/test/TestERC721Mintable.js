f1f6894593351dfd93b7cd15d9d8d822ca123a13
sameerdewan <sameer.dewan@itential.com>
2020-10-17 14:56:27 -0400

    match erc721 spec tests

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -1,36 +1,66 @@
-var ERC721Mintable = artifacts.require('HouseToken');
+const HouseToken = artifacts.require('HouseToken');
+const truffleAssert = require('truffle-assertions');
 
-contract('TestERC721Mintable', accounts => {
+contract('Test HouseToken', accounts => {
 
     const account_one = accounts[0];
     const account_two = accounts[1];
+    const account_three = accounts[2];
+    const account_four = accounts[3];
+
     const tokenName = 'HouseToken';
     const tokenSymbol = 'HOUSE';
-    const addressInvalid0x = '0x0000000000000000000000000000000000000000';
+    const tokens = [...Array(10).keys()];
     const tokenBaseURI = 'https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/';
+
+    let instance;
+    let owner;
 
     describe('match erc721 spec', function () {
         beforeEach(async function () { 
-            this.contract = await ERC721Mintable.new(tokenName, tokenSymbol, {from: account_one});
-
+            instance = await HouseToken.new(tokenName, tokenSymbol, {from: account_one});
+            owner = account_one;
             // TODO: mint multiple tokens
+            for (let index = 0; index < tokens.length; index++) {
+                const token = tokens[index];
+                await instance.mint(account_two, token, { from: owner });
+            }
+        });
+
+        it('should return total supply', async function () { 
+            const totalSupply = Number(await instance.totalSupply.call({from: owner}));
+            assert.equal(totalSupply, tokens.length, 'Error: Total Supply returned does not match tokens length');
+        });
+
+        it('should get token balance', async function () { 
+            const account_two_bal = await instance.balanceOf(account_two);
+            assert.equal(account_two_bal, tokens.length);
         })
 
-        // it('should return total supply', async function () { 
-            
-        // })
-
-        // it('should get token balance', async function () { 
-            
-        // })
-
         // // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
-        // it('should return token uri', async function () { 
-            
-        // })
+        it('should return token uri', async function () { 
+            const House1 = tokens[0];
+            const account_two_uri_1 = await instance.tokenURI(House1);
+            assert.equal(account_two_uri_1, tokenBaseURI + House1); 
 
-        // it('should transfer token from one owner to another', async function () { 
-            
+            const House2 = tokens[1];
+            const account_two_uri_2 = await instance.tokenURI(House2);
+            assert.equal(account_two_uri_2, tokenBaseURI + House2); 
+        })
+
+        it('should transfer token from one owner to another', async function () { 
+            const House1 = tokens[0];
+            const House2 = tokens[1];
+
+            const tx = await instance.transferFrom(account_two, account_three, House1, { from: account_two });
+            truffleAssert.eventEmitted(tx, 'Transfer');
+            const House1Owner = await instance.ownerOf(House1);
+            assert.equal(House1Owner, account_three);
+
+            const tx2 = await instance.transferFrom(account_two, account_four, House2, { from: account_two });
+            truffleAssert.eventEmitted(tx2, 'Transfer');
+            const House2Owner = await instance.ownerOf(House2);
+            assert.equal(House2Owner, account_four);
         })
     });
 
