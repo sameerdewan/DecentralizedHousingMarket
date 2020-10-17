26f065448f120d6bc1b19d08cbf70c01460385b8
sameerdewan <sameer.dewan@itential.com>
2020-10-17 16:45:40 -0400

    remove .only

--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,12 +1,12 @@
 const SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
 const Verifier = artifacts.require('Verifier');
+const truffleAssert = require('truffle-assertions');
 const json = require('../../zokrates/code/square/proof.json');
 
 contract('Test Verifier and SolnSquareVerifier', accounts => {
     const account_one = accounts[0];
     const tokenName = 'HouseToken';
     const tokenSymbol = 'HOUSE';
-    const tokenBaseURI = 'https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/';
 
     const A = json.proof.a;
     const B = json.proof.b;
@@ -14,7 +14,7 @@ contract('Test Verifier and SolnSquareVerifier', accounts => {
     const inputs = json.inputs;
 
     // Test if a new solution can be added for contract - SolnSquareVerifier
-    describe.only('Test if a new solution can be added for contract - SolnSquareVerifier', () => {
+    describe('Test if a new solution can be added for contract - SolnSquareVerifier', () => {
         let verifier;
         let solnSquareVerifier;
         let owner;
@@ -26,9 +26,8 @@ contract('Test Verifier and SolnSquareVerifier', accounts => {
 
         it('Can add a new solution for contract - SolnSquareVerifier', async () => {
             const tx = await solnSquareVerifier.addSolution(A, B, C, inputs, { from: owner });
+            truffleAssert.eventEmitted(tx, 'SolutionAdded');
         });
     });
-
     // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
-
 });
